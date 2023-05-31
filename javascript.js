20f4d05de6bca1c3982469ab564d40174772cf91
Ana Elisa <espindola.aelisa@gmail.com>
2023-05-31 16:47:47 -0300

    fix form entradas

--- javascript.js
+++ javascript.js
@@ -1,20 +1,20 @@
 function calcularTotal(){
-    let cant = document.getElementById('cantidad').value;
-    var cat = document.getElementById('categoria').value;
-    let final = calcularDescuento(cant, cat);
-    document.getElementById('precioFinal').innerText = final;
+    let cant = document.getElementById('cantidad').value; // capturo input Cantidad
+    var cat = document.getElementById('categoria').value; // capturo Categoria 
+    let final = calcularDescuento(cant, cat); // llamo a Fn calcularDescuento y le paso los valores
+    document.getElementById('precioFinal').innerText = final;  // muestro final con descuento aplicado
     
 };
 
 
 function calcularDescuento(a, b){
-    if (b === '1'){ //Estudiante descuenta 80% / 200
-        return a*(200-(200*0.8));
+    if (b === '1'){ // 1- Estudiante descuenta 80%
+        return a*(200-(200*0.8)); 
     }
-    if (b === '2'){ //Trainee descuenta 50% / 200
+    if (b === '2'){ // 2- Trainee descuenta 50%
         return a*(200-(200*0.5));
     }
-    if (b === '3'){ //Jr descuenta 15% / 200
+    if (b === '3'){ // 3- Jr descuenta 15%
         return a*(200-(200*0.15));
     }
 };
